ddd3b6b293745ef64559ab700a85deb88344f000
leogomes57 <leo.gomes57@hotmail.com>
2022-06-02 19:26:48 -0300

    Resolvendo smells

--- src/controllers/eventos.js
+++ src/controllers/eventos.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
     const id = parseInt(req.params.id);
     Eventos.buscarPorId(id)
       .then((resultado) => {
-        if (resultado) {
+        if (resultado.length) {
           res.json(resultado);
         } else {
           res.status(404).end();
